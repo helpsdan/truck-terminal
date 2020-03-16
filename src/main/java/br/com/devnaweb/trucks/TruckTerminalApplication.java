96013d285076d6fe11865f6b7da5c6c620aef4dd
Daniel Aguiar Alves <T1111DNL@prestadorcbmp.com.br>
2020-03-16 17:08:26 -0300

    Create project and define dependencies.
    Task: https://trello.com/c/JZTqqxBK

---
+++ src/main/java/br/com/devnaweb/trucks/TruckTerminalApplication.java
@@ -0,0 +1,13 @@
+package br.com.devnaweb.trucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckTerminalApplication.class, args);
+	}
+
+}
