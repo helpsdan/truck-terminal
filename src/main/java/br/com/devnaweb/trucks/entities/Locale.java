b4fe85f49932a993786901b56da0151a8f307624
Daniel Aguiar Alves <T1111DNL@prestadorcbmp.com.br>
2020-03-17 00:20:36 -0300

    Implements repositories.
    TASK: https://trello.com/c/qcAP0Oqn

--- src/main/java/br/com/devnaweb/trucks/entities/Locale.java
+++ src/main/java/br/com/devnaweb/trucks/entities/Locale.java
@@ -7,14 +7,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "LOCALE")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -27,12 +32,17 @@ public class Locale {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "LOCALE_ID")
+    @JoinColumn(name = "DRIVER_ID")
     private Driver driver;
 
+    @Basic
+    @Enumerated
     private LocaleType localeType;
 
+    @Basic
+    @Column(name = "LONGITUDE", nullable = false)
     private Long longitude;
 
+    @Column(name = "LATITUDE", nullable = false)
     private Long latitude;
 }
